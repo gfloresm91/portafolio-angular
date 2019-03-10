6f0b5a6fc104bda95781f7a6f28f6079f237c4a1
Gabriel Flores <gabrielfloresmonsalve@gmail.com>
2019-03-10 00:12:08 -0300

    Se agrega método para confirmar la eliminación de un proyecto

--- src/app/components/detail/detail.component.ts
+++ src/app/components/detail/detail.component.ts
@@ -13,6 +13,7 @@ import { Router, ActivatedRoute, Params } from '@angular/router';
 export class DetailComponent implements OnInit {
   public url: string;
   public project: Project;
+  public confirm: boolean;
 
   constructor(
     private _projectService: ProjectService,
@@ -20,6 +21,7 @@ export class DetailComponent implements OnInit {
     private _route: ActivatedRoute
   ) {
     this.url = Global.url;
+    this.confirm = false;
   }
 
   ngOnInit() {
@@ -38,6 +40,10 @@ export class DetailComponent implements OnInit {
         console.log(error as any);
       }
     );
+  }
+
+  setConfirm(confirm) {
+    this.confirm = confirm;
   }
 
   deleteProject(id) {
